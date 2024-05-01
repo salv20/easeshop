b74e21b830076f7eb18f1445810f80cad23d9fd5
Salvation Amoke <salvationamoke@gmail.com>
2024-05-01 03:17:25 +0100

    nav bag design

--- src/pages/landingpage/Nav.jsx
+++ src/pages/landingpage/Nav.jsx
@@ -1,7 +1,43 @@
+import { Link } from "react-router-dom";
+import { FaCartShopping } from "react-icons/fa6";
+
 const Nav = () => {
   return (
-    <header>
-      <h1>header foe all man</h1>
+    <header className="text-[#ddddde] py-4 items-center gap-6 flex justify-between">
+      <h1 className="uppercase font-bold">easestore</h1>
+
+      <div className="hidden lg:flex gap-4">
+        <button className="btn-sm">
+          <Link>home</Link>
+        </button>
+        <button className="btn-sm">
+          <Link>shop</Link>
+        </button>
+        <button className="btn-sm">
+          <Link>sale</Link>
+        </button>
+        <button className="btn-sm">
+          <Link>blog</Link>
+        </button>
+        <button className="btn-sm">
+          <Link>showcase</Link>
+        </button>
+      </div>
+
+      <div className="flex gap-2 sm:gap-4">
+        <button className="text-[#cdcdd1] relative bg-[#252526] px-2.5 py-1.5 items-center rounded-full">
+          <Link>
+            <FaCartShopping className="relative z-40" />
+            <span className="absolute top-0 h-4 w-4 bg-[#858585] rounded-full z-10"></span>
+          </Link>
+        </button>
+        <button className="btn-sm">
+          <Link>login</Link>
+        </button>
+        <button className="btn-sm">
+          <Link>signup</Link>
+        </button>
+      </div>
     </header>
   );
 };

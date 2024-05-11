3c6593c80a169980e75adaba65aa78e24aace993
Salvation Amoke <salvationamoke@gmail.com>
2024-05-11 22:27:09 +0100

    options button

--- src/pages/landingpage/Collections.jsx
+++ src/pages/landingpage/Collections.jsx
@@ -5,16 +5,19 @@ import { useEffect } from "react";
 const Collections = ({ products, fetchProduct }) => {
   useEffect(() => {
     fetchProduct();
-    console.log(products);
-    if (products === undefined) {
-      console.log("hello");
-    }
   }, []);
 
   return (
-    <div>
+    <section>
+      <div className="text-white grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 justify-center w-[80%] mx-auto">
+        <button className="sort-btn btn-sm">all</button>
+        <button className="sort-btn btn-sm">men&#39;s clothing</button>
+        <button className="sort-btn btn-sm">women&#39;s clothing</button>
+        <button className="sort-btn btn-sm">jewerly</button>
+        <button className="sort-btn btn-sm">electronics</button>
+      </div>
       <h1> collletion</h1>
-    </div>
+    </section>
   );
 };
 
